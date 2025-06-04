f163878c577111323dc17aeacb734e1847b7f0fa
Amit Ghatage <amitghatage001@gmail.com>
2025-06-04 23:53:19 +0530

    Springboot maven endpoint created for integration with database

---
+++ src/main/java/com/dynamoDB/integration/DynamoDbDatabaseIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.dynamoDB.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamoDbDatabaseIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamoDbDatabaseIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dynamoDB/integration/DynamoDbDatabaseIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamoDB.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoDbDatabaseIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
